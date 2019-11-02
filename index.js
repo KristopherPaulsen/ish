894e92909ed52b5161d0c3466477c5ab2dee240c
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2019-11-02 07:06:18 -0600

    Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const Fuse = require('fuse.js');
+
+const main = (data) => {
+  const string = process.argv.slice(2)[0];
+
+  const list = data.toString().split('\n');
+
+  const fuse = new Fuse(
+    list.map(item => ({ item })),
+    { keys: ['item'] },
+  );
+
+  console.log(fuse.search(string)[0].item);
+}
+
+process.stdin.on('data', main);
